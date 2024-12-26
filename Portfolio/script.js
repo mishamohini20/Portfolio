6221f0670fc30b23ec1b6c07e1567f5f335eb475
Aishwarya Bharti <156784412+mishamohini20@users.noreply.github.com>
2024-12-26 14:13:49 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
